151c6056bf99c630df30c2c0aeef4ae7ade03612
Allan <allanfelipedk@gmail.com>
2023-02-04 16:00:45 -0300

    If Statement - How to Use the Truth Values with Relational Operators

---
+++ If Statement/If Statement/If Statement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 4;
+    int c = 6;
+    if (a == b)
+    {
+        cout << "Hello World";
+    }
+    else
+    {
+        cout << "a ! b";
+    }
+    cout << "\n";
+
+    system("pause");
+}
+
