e7eb624bf37cffeb7e3e3c3eedbbcfba4410236b
Allan <allanfelipedk@gmail.com>
2023-02-06 23:21:53 -0300

    Else If - Adding Even More Functionality to your If Statements

--- If Statement/If Statement/If Statement.cpp
+++ If Statement/If Statement/If Statement.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int a = 1;
-    int b = 13;
+    int a = 5;
+    int b = 5;
     int c = 6;
+
     if (b < a)
     {
         cout << "a is less than b" << endl;
     }
-    else
+    else if(a < b)
     {
         cout << "b is not less than a " << endl;
+    }
+    else if(a == b)
+    {
+        cout << "a is equal to b" << endl;
+    }
+    else
+    {
+        cout << "ERRO 404!!" << endl;
     }
 
     system("pause");
