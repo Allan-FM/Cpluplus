157b8d3b1f64b30cbf81aa2c10e43d0c3d733a04
Allan <allanfelipedk@gmail.com>
2023-02-13 20:25:11 -0300

    Increment Operators - Hands On Practice With Computations

---
+++ Increment Operators/Increment Operators/Increment Operators.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int i = 3;
+	/*
+	i += 1;
+	cout << i << endl;
+	i -= 2;
+
+	cout << i << endl;
+	i *= 3;
+
+	cout << i << endl;
+	i /= 4;
+
+	cout << i << endl;
+	i %= 5;
+
+	cout << i << endl;
+	*/
+
+	/*
+		float j = 2.0;
+		float myFloat = j / i;
+		cout << "My float -> " << myFloat << endl;
+	*/
+
+	/*
+	--i;
+	cout << i << endl;
+	i++;
+	cout << i++ << endl;
+	*/
+
+	int j = i++;
+	cout << i++ << endl;
+
+	system("pause");
+}
